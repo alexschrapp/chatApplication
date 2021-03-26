fb59034a38590b7c9ec55251a33ead7904cbbf5e
alexschrapp <70139188+alexschrapp@users.noreply.github.com>
2021-03-26 15:44:26 +0200

    fixed issue with color

--- app/src/main/java/chat/application/assignment/Settings.kt
+++ app/src/main/java/chat/application/assignment/Settings.kt
@@ -4,15 +4,21 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ActionCodeEmailInfo
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class Settings : AppCompatActivity() {
     @SuppressLint("RestrictApi")
+    private lateinit var curEmail: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        curEmail = findViewById(R.id.email)
 
         supportActionBar?.apply {
             title="Settings"
@@ -21,7 +27,7 @@ class Settings : AppCompatActivity() {
         }
 
         val currentUser = intent.getParcelableExtra<FirebaseUser>("currentUser")
-        //email.text = currentUser?.email
+        curEmail.text = currentUser?.email
 
     }
 
@@ -32,7 +38,7 @@ class Settings : AppCompatActivity() {
 
     fun signOut(view: View){
         FirebaseAuth.getInstance().signOut()
-        //email.text = ""
+        curEmail.text = ""
     }
 
 }
